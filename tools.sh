0eb7de083826c10b71c9907b198b62d0d6e80ed0
ArcherFX <mother2110@users.noreply.github.com>
2022-04-06 10:45:36 -0700

    Update tools.sh

--- tools.sh
+++ tools.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
-echo " Installing tools..."
+Yellow="\e[33m"
+ENDCOLOR="\e[0m"
+
+echo -e "${Yellow}Installing Tools...${ENDCOLOR}"
 
 cd /opt
-
-echo "Grabbing lnkbomb..."
+0
+echo -e "${Yellow}lnkbomb${ENDCOLOR}"
 git clone https://github.com/dievus/lnkbomb.git
 
-echo "Instslling threasder3000..."
+echo -e "${Yellow}Instslling threasder3000...${ENDCOLOR}"
 pip3 install threader3000
 
-echo "Installing DNS Scanner..."
+echo -e "${Yellow}Instslling DNS Scanner...${ENDCOLOR}"
 
 git clone https://github.com/dievus/msdnsscan.git
 cd msdnsscan
@@ -18,20 +21,16 @@ pip3 install -r requirements.txt
 
 cd /opt
 
-echo "Grabbing NameMash pyhton script..."
+echo -e "${Yellow}Downloading Namemash${ENDCOLOR}"
 
 wget https://gist.githubusercontent.com/superkojiman/11076951/raw/053152dba6c5cc3107ff0374ce7246306958d72c/namemash.py
 
-echo "Instllling Terminator Terminal..."
-
-apt install terminator -y
-
-echo "Installing Oh365UserFinder"
+echo -e "${Yellow}Installing oh365UserFinder${ENDCOLOR}" 
 
 git clone https://github.com/dievus/Oh365UserFinder.git
 pip3 install -r Oh365UserFinder/requirements.txt
 
-echo "Installing ReconFTW"
+echo -e "${Yellow}Installing ReconFTW...${ENDCOLOR}"
 git clone https://github.com/six2dez/reconftw
 cd reconftw/
 ./install.sh
