e3a03ef2ca1b239ce584a51c1050b7fa03e39040
ArcherFX <mother2110@users.noreply.github.com>
2022-04-06 13:07:39 -0700

    Update tools.sh

--- tools.sh
+++ tools.sh
@@ -42,6 +42,11 @@ echo -e "${Yellow}Downloading Namemash${ENDCOLOR}"
 
 wget https://gist.githubusercontent.com/superkojiman/11076951/raw/053152dba6c5cc3107ff0374ce7246306958d72c/namemash.py
 
+echo -e "${Yellow}Installing CME${ENDCOLOR}"
+python3 -m pip install pipx
+pipx ensurepath
+pipx install crackmapexec
+
 echo -e "${Yellow}Installing oh365UserFinder${ENDCOLOR}" 
 
 git clone https://github.com/dievus/Oh365UserFinder.git
@@ -49,11 +54,6 @@ cd oh365UserFinder
 pip3 install -r Oh365UserFinder/requirements.txt
 
 cd /opt
-
-echo -e "${Yellow}Installing CME${ENDCOLOR}"
-python3 -m pip install pipx
-pipx ensurepath
-pipx install crackmapexec
 
 echo -e "${Yellow}Installing ReconFTW...${ENDCOLOR}"
 git clone https://github.com/six2dez/reconftw
